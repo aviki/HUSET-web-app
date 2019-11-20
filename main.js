a632cbdba0fafa2182c00322ec1020143529e513
aviki <toldi.victoria@gmail.com>
2019-11-20 19:28:14 +0100

    fetch data + create modal

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 
-//when the page loads, call the init function
-window.addEventListener("DOMContentLoaded", init);
-
-function init(){
-const urlParams = new URLSearchParams(window.location.search);
-//grab search=something from the url (it might not exist)
-const search = urlParams.get("search");
-//grab id=something from the url (it might not exist)
-const id = urlParams.get("id");
-const category = urlParams.get("category");
+window.addEventListener("DOMContentLoaded", checkData);
+const modal = document.querySelector(".modalbg");
+//modal.style.display = "none";
+function checkData() {
+    if (document.querySelector("#theater")) {
+        getData();
+    }
+}
 
 
 
-function getTheaterEvent() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const id = urlParams.get("id");
-
-  fetch("http://liatalony.com/liatalony/wp-json/wp/v2/event/"+id)
+  fetch("http://liatalony.com/liatalony/wp-json/wp/v2/event?_embed&per_page=100&categories=15")
     .then(res=>res.json())
-  .then(showEvent)
+    .then(handleData)
 
-  function showEvent(event){
-    documet.querySelector("article h1").textContent=event.title.rendered
+  function handleData(myData) {
+    // console.log(myData);
+    // 1 loop
+    myData.forEach(showTheater)
   }
+
+  function showTheater(theater) {
+  //console.log(event)
+
+  const imgPath = theater._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    const template = document.querySelector("template").content;
+    const templateCopy = template.cloneNode(true);
+    const imgBG = templateCopy.querySelector(".img");
+    const h1 = templateCopy.querySelector("article h1");
+    const h2 = templateCopy.querySelector("h2");
+    const article = templateCopy.querySelector("article");
+    const a = templateCopy.querySelector("a");
+    const price = templateCopy.querySelector(".price");
+
+
+    imgBG.style.backgroundImage = `url(${imgPath})`;
+    h1.textContent = theater.title.rendered;
+    h2.textContent = theater.event_date;
+    price.textContent = theater.price;
+    article.addEventListener("click", function () {
+        location.href = "sub.html?id=" + theater.id;
+  })
+  document.querySelector("#theater").appendChild(templateCopy);
 }
---
+++ script.js
@@ -0,0 +1,92 @@
+window.addEventListener("DOMContentLoaded", checkData);
+const modal = document.querySelector(".modalbg");
+//modal.style.display = "none";
+function checkData() {
+    if (document.querySelector("#theater")) {
+        getData();
+    }
+}
+//Fetchinn the theater events from WordPress//
+function getData() {
+
+    fetch("http://liatalony.com/liatalony/wp-json/wp/v2/event?_embed&per_page=100&categories=15")
+        .then(res => res.json())
+        .then(handleData)
+}
+
+function handleData(myData) {
+    //    console.log(myData);
+    //1 loop
+    myData.forEach(showTheater)
+
+
+
+}
+
+function showTheater(theater) {
+    //    console.log(theater)
+
+    const imgPath = theater._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    //
+    //
+    const template = document.querySelector("template").content;
+    const theaterCopy = template.cloneNode(true);
+    //    //3. textcontent & innerHTML
+    const article = theaterCopy.querySelector("article");
+
+    const headlinefront = theaterCopy.querySelector(".headlinefront");
+    headlinefront.textContent = theater.title.rendered;
+    const timefront = theaterCopy.querySelector(".timefront");
+    timefront.textContent = theater.event_time;
+    const datefront = theaterCopy.querySelector(".datefront");
+    datefront.textContent = theater.event_date;
+    const locationfront = theaterCopy.querySelector(".locationfront");
+    locationfront.textContent = theater.where;
+    const pricefront = theaterCopy.querySelector(".pricefront");
+    pricefront.textContent = theater.price;
+    const container = theaterCopy.querySelector("container");
+    const content = theaterCopy.querySelector("content");
+    const img = theaterCopy.querySelector(".img");
+    img.style.backgroundImage = `url(${imgPath})`;
+
+    //Creating the modal for single theater events// 
+    article.addEventListener("click", function () {
+        console.log(theater);
+        const modal = document.querySelector(".modalbg");
+        modal.style.display = "block";
+        const modalheadlinefront = document.querySelector(".modalheadlinefront");
+        modalheadlinefront.textContent = theater.title.rendered;
+        const modaltimefront = document.querySelector(".modaltimefront");
+        modaltimefront.textContent = theater.event_time;
+        const modaldatefront = document.querySelector(".modaldatefront");
+        modaldatefront.textContent = theater.event_date;
+        const modallocationfront = document.querySelector(".modallocationfront");
+        modallocationfront.textContent = theater.where;
+        const modalpricefront = document.querySelector(".modalpricefront");
+        modalpricefront.textContent = theater.price;
+        const modalimg = document.querySelector(".modalimg");
+        modalimg.style.backgroundImage = `url(${imgPath})`;
+        const modaldescription = document.querySelector(".modaldescription")
+        modaldescription.innerHTML = theater.content.rendered;
+        const body = document.querySelector("body");
+        body.classList.add("modalopen");
+        const exit = document.querySelector(".exit");
+        exit.addEventListener("click", function () {
+            modal.style.display = "none";
+            body.classList.remove("modalopen");
+        });
+    });
+
+    document.querySelector("#theater").appendChild(theaterCopy)
+
+}
+
+//const menu = document.querySelector(".menu");
+//const nav = document.querySelector("nav");
+//const navexit = document.querySelector(".navexit");
+//menu.addEventListener("click", function () {
+    //nav.style.width = "50%";
+//});
+//navexit.addEventListener("click", function () {
+  //  nav.style.width = "0%";
+//});

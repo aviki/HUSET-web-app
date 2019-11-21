dbfc9042eadf5138e14b5eb4c8902e5e0571555f
aviki <toldi.victoria@gmail.com>
2019-11-21 22:29:18 +0100

    styled menu modal

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function handleData(myData) {
 }
 
 function showTheater(theater) {
-      console.log(theater);
+      console.log(showTheater);
 
     const imgPath = theater._embedded["wp:featuredmedia"][0].media_details.sizes.full.source_url;
     //
@@ -52,7 +52,7 @@ function showTheater(theater) {
 
     //Creating the modal for single theater events//
     article.addEventListener("click", function () {
-        console.log(theater);
+
         const modal = document.querySelector(".modalbg");
         modal.style.display = "block";
         const modalheadlinefront = document.querySelector(".modalheadlinefront");
@@ -80,4 +80,34 @@ function showTheater(theater) {
 
     document.querySelector("#theater").appendChild(theaterCopy)
 
+}
+
+//Hamburger menu modal//
+
+// Get the modal
+var menuModal = document.getElementById("myMenu");
+
+// Get the button that opens the modal
+var checkbox = document.getElementById("toggle");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+checkbox.onclick = function() {
+  menuModal.style.display = "block";
+  menuModal.style.zIndex = "100";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  menuModal.style.display = "none";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == menuModal) {
+    menuModal.style.display = "none";
+  }
 }
